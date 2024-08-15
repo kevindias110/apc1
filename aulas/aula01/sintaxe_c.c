afb07b0b27855bca2d3a119c3d0d44dbfe09ff5d
kevindias110 <kevin.dias@iesb.edu.br>
2024-08-15 00:48:14 +0000

    codigos aula 01

---
+++ aulas/aula01/sintaxe_c.c
@@ -0,0 +1,46 @@
+// ISSO eh diferente de isso
+
+//area_retangulo = base * altura;
+
+//main( ) {
+
+  //linha_de_codigo_terminado_em_ponto_e_virgula;
+
+ // comando1;
+  //comando2;
+
+  
+
+
+/* bloco_de_comentario */
+
+#include <stdio.h> // inclusao de blibliotecas
+
+int main()  // função principal
+// seu codigo aqui
+{
+// 1 entrada
+
+int primeiro_numero;
+ int segundo_numero;
+
+primeiro_numero = 1;
+segundo_numero = 2;
+// levar meus ingredientes
+//char, int, float, double, long, long long, unsigned, short, unsigned short, unsigned long, unsigned long long
+// variaveis
+//leitura
+
+// 2 processamento
+// calculo soma, subtracao, nultiplicacao, divisao
+ int soma = primeiro_numero + segundo_numero;
+
+
+// 3 saida
+// escrita
+printf("A soma de %i com %i eh %i\n", primeiro_numero,segundo_numero,soma);
+
+
+return 0;  // retorno fim do progama
+
+}
